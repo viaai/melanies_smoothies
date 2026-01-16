1673f01255abea7becf98473c8cc646fdfbf1306
vIA.AI <123186689+viaai@users.noreply.github.com>
2026-01-15 19:29:49 -0500

    Update streamlit_app_customize.py

--- streamlit_app_customize.py
+++ streamlit_app_customize.py
@@ -1,7 +1,9 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize your smoothie :cup_with_straw:")
